1e48b9b85e0b6e7da75116cf878352aab0954b1d
SahithiUppala <sahithiuppala947@gmail.com>
2025-10-25 00:05:55 +0530

    Fix issue #61:corrected background color to #fb923c

--- src/components/buttons/Button57.js
+++ src/components/buttons/Button57.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button57 = ({ text = "Button 57" }) => {
   return (
-        <button className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full border-t-4 border-orange-600 transition duration-300">
+        <button className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded-full border-t-4 border-orange-500 transition duration-300">
       {text}
     </button>
   );
