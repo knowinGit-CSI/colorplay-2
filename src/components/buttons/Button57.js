5961a9676c2cf5617a285e0a9ffa50c6d986ed97
SahithiUppala <sahithiuppala947@gmail.com>
2025-10-24 23:52:00 +0530

    Changed button color & hover color for Button57

--- src/components/buttons/Button57.js
+++ src/components/buttons/Button57.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button57 = ({ text = "Button 57" }) => {
   return (
-    <button className="#C4C4C4 hover:bg-lime-800 text-white font-bold py-2 px-4 rounded-full border-t-4 border-lime-500 transition duration-300">
+    <button className="#E3735E	 hover:#E3735E	text-white font-bold py-2 px-4 rounded-full border-t-4 border-lime-500 transition duration-300">
       {text}
     </button>
   );
