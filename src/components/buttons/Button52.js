43419010233faae5dcfdefe88542083f8ebbbeb3
jasvini123 <jasvini8989@gmail.com>
2025-10-26 05:38:33 +0530

    Changed text font weight for issue #74

--- src/components/buttons/Button52.js
+++ src/components/buttons/Button52.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button52 = ({ text = "Button 52" }) => {
   return (
-    <button className="bg-cyan-700 hover:bg-cyan-800 text-white font-medium py-3 px-5 rounded-full ring-2 ring-cyan-400 transition duration-300">
+    <button className="bg-cyan-700 hover:bg-cyan-800 text-white font-bold py-3 px-5 rounded-full ring-2 ring-cyan-400 transition duration-300">
       {text}
     </button>
   );
