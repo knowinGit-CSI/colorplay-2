fbd4d2acfd59691e680e9ccbe7f24d215bdf47ea
Saanvi577 <24071a05a0@vnrvjiet.in>
2025-10-26 16:29:44 +0530

    Fix issue #76 : modified  border radius from lg to 2xl

--- src/components/buttons/Button54.js
+++ src/components/buttons/Button54.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button54 = ({ text = "Button 54" }) => {
   return (
-    <button className="bg-rose-700 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded-lg border-b-4 border-rose-500 transition duration-300">
+    <button className="bg-rose-700 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded-2xl border-b-4 border-rose-500 transition duration-300">
       {text}
     </button>
   );
