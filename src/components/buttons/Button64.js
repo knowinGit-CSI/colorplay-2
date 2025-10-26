dada54b6a9d68841d3e4026d29c97163ad1366e0
sairamareddy2 <mallidisairamareddy40@gmail.com>
2025-10-26 15:01:40 +0530

    fixed button  #64:changed hover scale

--- src/components/buttons/Button64.js
+++ src/components/buttons/Button64.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button64 = ({ text = "Button 64" }) => {
   return (
-    <button className="bg-rose-600 hover:bg-rose-700 text-white font-semibold py-2 px-4 rounded-lg shadow-inner transition duration-300">
+    <button className="bg-rose-600 hover:bg-rose-700 text-white font-semibold py-2 px-4 rounded-lg shadow-inner transition duration-300 transform hover:scale-105">
       {text}
     </button>
   );
